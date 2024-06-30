667207d94b4830b5d8296212a6eb5601a7a51ff4
deathface77 <face0death77@gmail.com>
2024-06-30 13:34:58 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let slideIndex = 1;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 20000); // Change image every 15 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
